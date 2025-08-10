198f1d429adf0a3a4902a0c613f14fafca4d9349
chandra parmar <chandra.parmar2004@gmail.com>
2025-08-10 20:41:08 +0530

    course details function added

--- controllers/Course.js
+++ controllers/Course.js
@@ -160,10 +160,66 @@ const getAllCourse = async(req,res)=>{
     }
 }
 
+//get full course details
+const getCourseDetails = async(req,res)=>{
+    try{
+       //get id
+       const {courseId} = req.body
+       //find course details
+       const courseDetails = await Course.find({_id:courseId})
+                                            .populate(
+                                                {
+                                                    path:"instructor",
+                                                    populate:{
+                                                        path:"additionalDetails"
+                                                    },
+                                                }
+                                            ).populate('category')
+                                            .populate('ratingAndreviews')
+                                            .populate({
+                                                path:"courseContent",
+                                                populate:{
+                                                    path:"subSection"
+                                                }
+                                            })
+                                            .exec()
+         //validation
+         if(!courseDetails)
+         {
+            return res.status(400).json(
+                {
+                    success:false,
+                    message:`Could not find the course with ${courseId}`
+                }
+            )
+         }
+         //return response
+
+         return res.status(200).json(
+            {
+                success:true,
+                message:"Course details fetched successfully",
+                data:courseDetails
+            }
+         )
+    }catch(error)
+    {
+        console.log(error)
+        return res.status(500).json(
+            {
+                success:false,
+                message:error.message
+            }
+        )
+
+    }
+}
+
 
 module.exports ={
     createCourse,
-    getAllCourse
+    getAllCourse,
+    getCourseDetails
 }
 
 
