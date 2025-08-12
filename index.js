49212100f2b4cc9492e1000804013dc3147a22e2
chandra parmar <chandra.parmar2004@gmail.com>
2025-08-12 14:09:13 +0530

    modified

--- index.js
+++ index.js
@@ -1,11 +1,56 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-require('dotenv').config()
-
 const port = process.env.PORT
-
 const dbConnect = require('./config/database')
+const cookieParser = require('cookie-parser')
+const cors= require('cors')
+const {cloudinaryConnect} = require('./config/cloudinary')
+const dotenv = require('dotenv')
+const userRoutes = require('./routes/User')
+const profileRoutes = require('./routes/')
+const fileUpload = require('express-fileupload')
+
+
+//database connect
 dbConnect()
+
+//middlewares
+app.use(express.json())
+app.use(cookieParser())
+app.use(
+    cors({
+        //frontend url 
+        origin:"http://localhost:3000",
+        credentials:true 
+    })
+)
+app.use(fileUpload({
+    useTempFiles:true,
+    tempFileDir:"/tmp"
+}))
+
+//cloudinary connection
+cloudinaryConnect()
+
+
+//mount routes
+app.use('/api/v1/auth',userRoutes)
+app.use('/api/v1/profile',profileRoutes)
+app.use('/api/v1/course',courseRoutes)
+app.use('/api/v1/payment',paymentRoutes)
+
+
+//default routes
+app.get('/',(req,res)=>{
+    return res.json(
+        {
+            success:true,
+            message:"Hello world"
+        }
+    )
+})
+
 
 
 app.listen(port,()=>{
