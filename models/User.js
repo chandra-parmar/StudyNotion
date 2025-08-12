2d0ad710ad46468430e58de0fb261137d4ff05fd
chandra parmar <chandra.parmar2004@gmail.com>
2025-08-12 10:41:13 +0530

    update user schema

--- models/User.js
+++ models/User.js
@@ -39,7 +39,7 @@ const userSchema = new mongoose.Schema(
       },
       courses:{
         type:mongoose.Schema.Types.ObjectId,
-        ref:"Courses"
+        ref:"Course"
       },
       image:{
         type:String,
