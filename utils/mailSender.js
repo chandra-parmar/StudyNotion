f63106b73186f04a58290a0864d03e9077ffc4d8
chandra parmar <chandra.parmar2004@gmail.com>
2025-08-15 13:05:14 +0530

    update mailSender function

--- utils/mailSender.js
+++ utils/mailSender.js
@@ -8,27 +8,32 @@ const mailSender = async(email,title,body)=>{
         let transporter = nodemailer.createTransport(
             {
                 host:process.env.MAIL_HOST,
+                port:465,
+                secure:true,
                 auth:{
                     user:process.env.MAIL_USER,
                     pass:process.env.MAIL_PASS
-                }
+                },
+                logger:true,
+                debug:true 
             }
         )
 
       //email send
       let info = await transporter.sendMail(
         {
-            from:'Studynotion || by chinu',
-            to:`${email}`,
-            subject:`${title}`,
-            html:`${body}`
+            from:`'Studynotion || by chinu' <${process.env.MAIL_USER}>`,
+            to:email,
+            subject:title,
+            html:body
         }
       )
-      console.log(info)
-    
+      console.log(info.messageId)
+       return info
     }catch(error)
     {
         console.log(error.message)
+        throw error
     }
 }
 
