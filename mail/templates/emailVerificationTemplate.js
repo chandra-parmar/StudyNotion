21419450831d75448b15d4b827827156c21edb12
chandra parmar <chandra.parmar2004@gmail.com>
2025-08-16 00:05:10 +0530

    varaiable name changes in email template

--- mail/templates/emailVerificationTemplate.js
+++ mail/templates/emailVerificationTemplate.js
@@ -1,4 +1,4 @@
-const otpTemplate = (otp) => {
+const otpTemplate = (OTP) => {
 	return `<!DOCTYPE html>
 	<html>
 	
@@ -73,7 +73,7 @@ const otpTemplate = (otp) => {
 				<p>Dear User,</p>
 				<p>Thank you for registering with StudyNotion. To complete your registration, please use the following OTP
 					(One-Time Password) to verify your account:</p>
-				<h2 class="highlight">${otp}</h2>
+				<h2 class="highlight">${OTP}</h2>
 				<p>This OTP is valid for 5 minutes. If you did not request this verification, please disregard this email.
 				Once your account is verified, you will have access to our platform and its features.</p>
 			</div>
