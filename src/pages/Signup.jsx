ba15b8da5ceb61a62d8d72498bc4de5122be4362
chandra parmar <chandra.parmar2004@gmail.com>
2025-09-06 13:01:32 +0530

    signup page added

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,18 @@
+import Template from '../components/core/Auth/Template'
+
+
+function Signup()
+{
+    return(
+        <Template
+        title="Join with StudyNotion for free"
+        description1="Build skills for today,tomorrow and beyound"
+        description2="Education to future-proof your carrer"
+        formType='signup'
+        >
+
+        </Template>
+    )
+}
+
+export default Signup 
