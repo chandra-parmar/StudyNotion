bacf86c82e009378847c73c9f95c968360fe5e78
chandra parmar <chandra.parmar2004@gmail.com>
2025-09-07 11:05:20 +0530

    login page created

---
+++ src/pages/Login.jsx
@@ -0,0 +1,16 @@
+
+import Template from "../components/core/Auth/Template"
+
+function Login() {
+  return (
+    <Template
+      title="Welcome Back"
+      description1="Build skills for today, tomorrow, and beyond."
+      description2="Education to future-proof your career."
+      
+      formType="login"
+    />
+  )
+}
+
+export default Login
