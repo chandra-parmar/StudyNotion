78a5ead7829e1901bff19caba4103cdcbec0c911
chandra parmar <chandra.parmar2004@gmail.com>
2025-09-16 12:08:07 +0530

    private route added

---
+++ src/components/core/Auth/PrivateRoute.jsx
@@ -0,0 +1,20 @@
+
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Navigate } from 'react-router-dom'
+
+const PrivateRoute =() =>{
+
+    const {token } = useSelector((state)=> state.auth)
+
+    if(token !== null)
+    {
+        return children
+    }
+    else{
+        return <Navigate to ='/login'></Navigate>
+    }
+   
+}
+
+export default PrivateRoute
