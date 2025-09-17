95283f36a06cd93832c631e7d608d648dcdab876
chandra parmar <chandra.parmar2004@gmail.com>
2025-09-17 13:43:10 +0530

    modify contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 import Footer from "../components/common/Footer"
-import ContactDetails from "../components/ContactPage/ContactDetails"
+//import ContactDetails from "../components/ContactPage/ContactDetails"
 import ContactForm from "../components/ContactPage/ContactForm"
 
 const Contact = () => {
