518f4e66b0522005ec1ab800c6f3031825317dd6
chandra parmar <chandra.parmar2004@gmail.com>
2025-09-17 13:44:35 +0530

    modify error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Error =()=>{
     return(
-        <div className='flex justify-center items-center text-3xl text-red'>
+        <div className='flex justify-center items-center text-3xl text-white'>
         Error 404 Not found
         </div>
     )
