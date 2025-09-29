def2cae1a6a65c1ed4fc044004272bbc9e1378d1
chandra parmar <chandra.parmar2004@gmail.com>
2025-09-29 19:14:42 +0530

    add course form added

---
+++ src/components/core/Dashboard/AddCourse/index.jsx
@@ -0,0 +1,18 @@
+import RenderSteps from "./RenderSteps"
+
+export default function AddCourse() {
+    return (
+        <>
+            <div className="text-white">
+                <div>
+                    <h1>Add Course</h1>
+                    <div>
+                        <RenderSteps />
+                    </div>
+                </div>
+
+               
+            </div>
+        </>
+    )
+}
