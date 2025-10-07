1434026a5827c2dbbccbfdebe1b8f31e3fb92b62
chandra parmar <chandra.parmar2004@gmail.com>
2025-10-07 13:32:23 +0530

    api connector modify

--- src/services/apiconnector.js
+++ src/services/apiconnector.js
@@ -1,15 +1,25 @@
-import axios from "axios"
+import axios from "axios";
 
-export const axiosInstance = axios.create({})
+export const axiosInstance = axios.create({
+  baseURL: process.env.REACT_APP_BASE_URL, // optional but recommended
+  withCredentials: true, // if you also use cookies
+});
 
-export const apiConnector = (method, url, bodyData = null, headers = {}, params = null) => {
-  return axiosInstance({
-    method,
-    url,
-    data: bodyData,
-    headers: {
-      ...headers, // only attach if provided
-    },
-    params,
-  })
-}
+export const apiConnector = async (method, url, bodyData = null, headers = {}, params = null) => {
+  try {
+    const response = await axiosInstance({
+      method,
+      url,
+      data: bodyData,
+      headers: {
+        "Content-Type": "application/json",
+        ...headers, // includes Authorization if passed
+      },
+      params,
+    });
+    return response;
+  } catch (error) {
+    console.error("API CONNECTOR ERROR:", error);
+    throw error;
+  }
+};
