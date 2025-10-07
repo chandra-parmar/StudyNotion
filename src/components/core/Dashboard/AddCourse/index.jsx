da686b24ae190bef50571979a1ddf05497a7e5fb
chandra parmar <chandra.parmar2004@gmail.com>
2025-10-07 13:33:58 +0530

    add course page added

--- src/components/core/Dashboard/AddCourse/index.jsx
+++ src/components/core/Dashboard/AddCourse/index.jsx
@@ -1,17 +1,19 @@
-import RenderSteps from "./RenderSteps"
+import RenderSteps from './RenderSteps'
 
-export default function AddCourse() {
-    return (
+export default function AddCourse()
+{
+    return(
         <>
             <div className="text-white">
                 <div>
                     <h1>Add Course</h1>
                     <div>
-                        <RenderSteps />
+                        <RenderSteps></RenderSteps>
                     </div>
                 </div>
-
-               
+                <div>
+                    <p>Code upload tips</p>
+                </div>
             </div>
         </>
     )
