df684046541a711a11a8e630c9cb39331f86c38f
chandra parmar <chandra.parmar2004@gmail.com>
2025-10-07 13:34:47 +0530

    render steps for course added

--- src/components/core/Dashboard/AddCourse/RenderSteps.jsx
+++ src/components/core/Dashboard/AddCourse/RenderSteps.jsx
@@ -1,56 +1,45 @@
-import React from 'react'
-import { FaCheck } from 'react-icons/fa';
-import { useSelector } from 'react-redux'
-import CourseBuilderForm from './CourseBuilder/CourseBuilderForm';
-import CourseInformationForm from './CourseInformation/CourseInformationForm';
+import { useSelector } from "react-redux";
+import CourseInformationForm from "./CourseInformation/CourseInformationForm";
+import { FaCheck } from "react-icons/fa";
 
 const RenderSteps = () => {
+  const { step } = useSelector((state) => state.course);
 
-    const {step} = useSelector((state)=> state.course);
-    console.log("current step value ",step)
-
-    const steps = [
-        {
-            id:1,
-            title: "Course Information",
-        },
-        {
-            id:2,
-            title: "Course Builder",
-        },
-        {
-            id:3,
-            title: "Publish",
-        },
-    ]
+  const steps = [
+    { id: 1, title: "Course Information" },
+    { id: 2, title: "Course Builder" },
+    { id: 3, title: "Publish" },
+  ];
 
   return (
     <>
-        <div>
-            {steps.map((item) => (
-                <div key={item.id}>
-                    <div className={`${step === item.id 
-                        ? "bg-yellow-900 border-yellow-50 text-yellow-50" 
-                        : "border-richblack-700 bg-richblack-800 text-richblack-300"}`}>
-                        {step > item.id ? (<FaCheck/>) : (item.id)}
-                    </div>
-                    {/* Add COde for dashes between the labels */}
-                </div>
-            ))}
-        </div>
-        <div>
-            {steps.map((item) => (
-                <div key={item.id}>
-                    <p>{item.title}</p>
-                </div>
-            ))}
-        </div>
+      <div>
+        {steps.map((item) => (
+          <div key={item.id}>
+            <div
+              className={`${
+                step === item.id
+                  ? "bg-yellow-900 border-yellow-50 text-yellow-50"
+                  : "border-richblack-700 bg-richblack-800 text-richblack-300"
+              }`}
+            >
+              {step > item.id ? <FaCheck /> : item.id}
+            </div>
+          </div>
+        ))}
+      </div>
 
-        {step === 1 && <CourseInformationForm />}
-        {step === 2 && <CourseBuilderForm/>}
-        {/* {step===3 && <PublishCourse/>} */}
+      {/* Current step title */}
+      <div>
+        <p>{steps.find((s) => s.id === step)?.title}</p>
+      </div>
+
+      {/* Forms */}
+      {step === 1 && <CourseInformationForm />}
+      {/* {step === 2 && <CourseBuilderForm />}
+          {step === 3 && <PublishCourse />} */}
     </>
-  )
-}
+  );
+};
 
-export default RenderSteps
+export default RenderSteps;
