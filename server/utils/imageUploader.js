06de4c2406e077322001736bfac55af748ca9d52
chandra parmar <chandra.parmar2004@gmail.com>
2025-10-10 19:43:34 +0530

    modify image uploader cloudinary function

--- server/utils/imageUploader.js
+++ server/utils/imageUploader.js
@@ -3,22 +3,22 @@ require('dotenv').config()
 
 
 //image uploader
-const uploadImageToCloudinary = async(file,folder,height,quality)=>
-{
-    const options ={folder}
+const uploadImageToCloudinary = async (file, folder, height, quality) => {
+  if (!file?.tempFilePath) {
+    throw new Error("File temp path is missing. Upload failed.");
+  }
 
-    if(height)
-    {
-      options.height = height
+  const options = { folder, resource_type: "auto" };
+  if (height) options.height = height;
+  if (quality) options.quality = quality;
 
-    }
-    if(quality)
-    {
-        options.quality= quality
-    }  
-    options.resource_type = "auto"
-
-    return await cloudinary.uploader.upload(file.tempFilePath,options)
-}
+  try {
+    const result = await cloudinary.uploader.upload(file.tempFilePath, options);
+    return result;
+  } catch (err) {
+    console.error("Cloudinary upload error:", err);
+    throw new Error("Cloudinary upload failed");
+  }
+};
 
 module.exports = uploadImageToCloudinary
