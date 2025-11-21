8369ab8195d470936fbbb5c32b094c2c6176ba43
chandra parmar <chandra.parmar2004@gmail.com>
2025-11-21 23:09:04 +0530

    edit course for instrcutor page added

---
+++ src/components/core/Dashboard/EditCourse/index.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
+import RenderSteps from '../AddCourse/RenderSteps'
+import { getFullDetailsOfCourse } from '../../../../services/operations/courseDetailsAPI'
+import {setCourse,setEditCourse} from '../../../../slices/courseSlice'
+import { useState } from 'react'
+
+export default function EditCourse(){
+
+    const dispatch = useDispatch()
+    const {courseId} = useParams()
+    const {course} = useSelector((state)=> state.course)
+    const [loading,setLoading] = useState(false)
+    const {token} = useSelector((state)=> state.auth)
+
+  useEffect(() => {
+  const populateCourseDetails = async () => {
+    setLoading(true);
+    const result = await getFullDetailsOfCourse(courseId, token);
+
+    if (result?.courseDetails) {
+      dispatch(setEditCourse(true));
+      dispatch(setCourse(result.courseDetails));
+    }
+
+    setLoading(false);
+  };
+
+  populateCourseDetails();
+}, []); // <-- IMPORTANT
+
+
+    if(loading)
+    {
+        return(
+            <div>
+                Loading...
+            </div>
+        )
+    }
+
+    return(
+        <div className='text-white'>
+          <h1>Edit Course</h1>
+          <div>
+            {
+                course ? (<RenderSteps></RenderSteps>) :(<p>Course not found</p>)
+
+            }
+          </div>
+        </div>
+    )
+}
+
