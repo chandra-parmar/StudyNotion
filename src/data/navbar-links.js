83d351af7d1aaf34926bbcc5a3b0335230c3ff78
chandra parmar <chandra.parmar2004@gmail.com>
2025-11-24 20:27:43 +0530

    nav bar links added

---
+++ src/data/navbar-links.js
@@ -0,0 +1,15 @@
+export const NavbarLinks = [
+  {
+    title: "Home",
+    path: "/",
+  },
+  {
+    title: "Catalog",
+     path: '/catalog',
+  },
+  {
+    title: "About Us",
+    path: "/about",
+  },
+
+];
