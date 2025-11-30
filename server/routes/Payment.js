a409c822dd01c73af7402153405a3ce8a8938b1c
chandra parmar <chandra.parmar2004@gmail.com>
2025-11-30 14:02:20 +0530

    verify payment and send payment success email routes added

--- server/routes/Payment.js
+++ server/routes/Payment.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-const {capturePayment,verifySignature } = require('../controllers/Payment')
-const {auth,isInstructor,isStudent, isAdmin} = require('../middlewares/auth')
+const {capturePayment,verifyPayment, sendPaymentSuccessEmail } = require('../controllers/Payment')
+const {auth,isStudent} = require('../middlewares/auth')
 
 //routes
 router.post('/capturePayment',auth,isStudent,capturePayment)
-router.post('/verifySignature',verifySignature)
+router.post('/verifyPayment',auth,isStudent,verifyPayment)
+router.post('/sendPaymentSuccessEmail',auth,isStudent,sendPaymentSuccessEmail)
 
 module.exports =router 
