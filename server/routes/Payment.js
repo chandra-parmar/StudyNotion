787def08fd04cb491202ad8edaddcc8259e5dabc
chandra parmar <chandra.parmar2004@gmail.com>
2025-12-03 11:24:20 +0530

    routes modify

--- server/routes/Payment.js
+++ server/routes/Payment.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-const {capturePayment,verifyPayment, sendPaymentSuccessEmail } = require('../controllers/Payment')
+const {capturePayment,verifyPayment, sendPaymentSuccessEmailController } = require('../controllers/Payment')
 const {auth,isStudent} = require('../middlewares/auth')
 
 //routes
 router.post('/capturePayment',auth,isStudent,capturePayment)
 router.post('/verifyPayment',auth,isStudent,verifyPayment)
-router.post('/sendPaymentSuccessEmail',auth,isStudent,sendPaymentSuccessEmail)
+router.post('/sendPaymentSuccessEmail',auth,isStudent,sendPaymentSuccessEmailController)
 
 module.exports =router 
