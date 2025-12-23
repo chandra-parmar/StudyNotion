e26cfd4c2e452227f1c83dab6c0faa392998554d
chandra parmar <chandra.parmar2004@gmail.com>
2025-12-23 10:27:29 +0530

    view course and videodetails route added

--- src/App.js
+++ src/App.js
@@ -22,6 +22,8 @@ import MyCourses from './components/core/Dashboard/MyCourses'
 import EditCourse from './components/core/Dashboard/EditCourse'
 import Catalog from './pages/Catalog'
 import CourseDetails from './pages/CourseDetails'
+import ViewCourse from './pages/ViewCourse'
+import VideoDetails from './components/core/ViewCourse/VideoDetails'
 
 
 function App()
@@ -104,6 +106,29 @@ function App()
           }
          
           </Route>
+
+         <Route element={
+              <PrivateRoute>
+                 <ViewCourse></ViewCourse>
+               </PrivateRoute>
+         }>
+
+         {
+            user?.accountType === ACCOUNT_TYPE.STUDENT && (
+              <>
+                <Route
+                 path='view-course/:courseId/section/:sectionId/sub-section/:subSectionId'
+                 
+                 element={<VideoDetails></VideoDetails>} 
+                 >
+
+                </Route>
+              </>
+            )
+         }
+
+         </Route>
+
 
           
 
