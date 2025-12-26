52f3e4bacbe0b4fd1f3e38e0c83f95122b9f0116
chandra parmar <chandra.parmar2004@gmail.com>
2025-12-26 18:48:28 +0530

    get instructor data endpoint added

--- src/services/api.js
+++ src/services/api.js
@@ -13,6 +13,7 @@ export const endpoints = {
 export const profileEndpoints = {
   GET_USER_DETAILS_API: BASE_URL + "/profile/getUserDetails",
   GET_USER_ENROLLED_COURSES_API: BASE_URL + "/profile/getEnrolledCourses",
+  GET_INSTRUCTOR_DATA_API:BASE_URL + "/profile/instructorDashboard"
 }
 
 // STUDENTS ENDPOINTS
@@ -69,3 +70,4 @@ export const settingsEndpoints = {
   CHANGE_PASSWORD_API: BASE_URL + "/auth/changepassword",
   DELETE_PROFILE_API: BASE_URL + "/profile/deleteProfile",
 }
+
