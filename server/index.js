490a7c110d94de9e3135806a52949cc50a8ecfb2
chandra parmar <chandra.parmar2004@gmail.com>
2025-12-26 18:40:08 +0530

    contact route added

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const userRoutes = require('./routes/User')
 const profileRoutes = require('./routes/Profile')
 const courseRoutes = require('./routes/Course')
 const paymentRoutes = require('./routes/Payment')
-
+const contactRoutes = require('./routes/Contact')
 
 
 
@@ -44,6 +44,7 @@ app.use('/api/v1/auth',userRoutes)
 app.use('/api/v1/profile',profileRoutes)
 app.use('/api/v1/course',courseRoutes)
 app.use('/api/v1/payment',paymentRoutes)
+app.use('/api/v1/reach',contactRoutes)
 
 
 //default routes
