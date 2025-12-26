697564a00ff27d50954d21db20ec804bb2f30f92
chandra parmar <chandra.parmar2004@gmail.com>
2025-12-26 18:32:43 +0530

    enroll student function modify

--- server/routes/Payment.js
+++ server/routes/Payment.js
@@ -1,12 +1,21 @@
-const express = require('express')
+// Import the required modules
+const express = require("express")
 const router = express.Router()
 
-const {capturePayment,verifyPayment, sendPaymentSuccessEmail } = require('../controllers/Payment')
-const {auth,isStudent} = require('../middlewares/auth')
+const {
+  capturePayment,
+  // verifySignature,
+  verifyPayment,
+  sendPaymentSuccessEmail,
+} = require('../controllers/Payment')
 
-//routes
-router.post('/capturePayment',auth,isStudent,capturePayment)
-router.post('/verifyPayment',auth,isStudent,verifyPayment)
-router.post('/sendPaymentSuccessEmail',auth,isStudent,sendPaymentSuccessEmail)
 
-module.exports =router 
+const { auth, isStudent} = require("../middlewares/auth")
+
+
+router.post("/capturePayment", auth, isStudent, capturePayment)
+router.post("/verifyPayment", auth, isStudent, verifyPayment)
+router.post("/sendPaymentSuccessEmail",auth,isStudent,sendPaymentSuccessEmail)
+// router.post("/verifySignature", verifySignature)
+
+module.exports = router
